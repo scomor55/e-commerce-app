32f2e8e34343a303712d30dee0dbd6c5dec7fe20
scomor55 <scomor2@etf.unsa.ba>
2024-09-15 04:09:24 +0200

    initial migration

--- Restaurant/obj/Debug/net8.0/Restaurant.AssemblyInfo.cs
+++ Restaurant/obj/Debug/net8.0/Restaurant.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7757f498dda693f00727a4600ff1ddea971d0f4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9846d742a30e67aea4c8a9bc31eb5495a4b224d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
