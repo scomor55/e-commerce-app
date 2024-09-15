9846d742a30e67aea4c8a9bc31eb5495a4b224d9
scomor55 <scomor2@etf.unsa.ba>
2024-09-15 03:58:22 +0200

    user error fixed

--- Restaurant/obj/Debug/net8.0/Restaurant.AssemblyInfo.cs
+++ Restaurant/obj/Debug/net8.0/Restaurant.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a8eed3ab38782d18229e49d5727f30e6826503e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7757f498dda693f00727a4600ff1ddea971d0f4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
